57fdeea2156db7e6573c02a366ba4a01cbf649e2
Trần Điền <dien.nguyen@cloudgo.vn>
2025-08-16 03:03:41 +0700

    mongodb_aggregate

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
+// import { mockData } from '~/apis/mock-data'
 import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
